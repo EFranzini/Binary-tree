e40ed1b5f102d4bc0e1eb16729abf2a2cecd8cf4
Enzo Franzini <enzo.franzinii@outlook.com>
2021-02-27 14:38:18 -0500

    0.2v

--- index.c
+++ index.c
@@ -1,7 +1,26 @@
 #include "stdio.h"
-#define ver 0.1
+#define ver 0.2
 
-void main()
+int main()
 {
-    printf("\n\nBinary tree version %f\n\n", ver);
+
+    int option = 0;
+
+    printf("\n\nBinary tree version %.1f\n\n", ver);
+
+    while (1)
+    {
+        printf("\n1 - Insert new value\n");
+        printf("2 - Track a number\n");
+        printf("8 - Credits\n");
+        printf("0 - Exit program\n");
+        printf("Please, insert one of the options above:");
+        scanf("%i", &option);
+
+        if (option == 0)
+        {
+            printf("\nBye!\n");
+            return 0;
+        }
+    }
 }
